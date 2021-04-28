d48e33708efa60ab4298340d6233bac0be65299b
dgk675 <dlinden@t-online.de>
2021-04-28 08:47:49 +0200

    Add some dummy buttons to the settings

--- lib/views/screens/settings_page.dart
+++ lib/views/screens/settings_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:moody/bloc/mood/mood_bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SettingsPage extends StatelessWidget {
   @override
@@ -7,8 +9,25 @@ class SettingsPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Settings'),
       ),
-      body: Center(
-        child: Text('Settings'),
+      body: ListView(
+        children: [
+          ListTile(
+            title: Text('Delete User Data'),
+            subtitle: Text('Permanently deletes all saved mood entries.'),
+            onTap: () {},
+          ),
+          ListTile(
+            title: Text('Export CSV'),
+            subtitle: Text('Coming soon: export your entries to a CSV file'),
+          ),
+          SwitchListTile(
+            title: Text('Dark Mode'),
+            subtitle: Text('Override the system theme.'),
+            // to be changed in DarkMode ticket.
+            value: true,
+            onChanged: (bool value) {},
+          ),
+        ],
       ),
     );
   }
