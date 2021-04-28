5e6db25f2dc842ad9384338f7cc0813697f7b156
dgk675 <dlinden@t-online.de>
2021-04-28 15:05:34 +0200

    switch to SliverAppBar for better UX on big device

--- lib/views/screens/settings_page.dart
+++ lib/views/screens/settings_page.dart
@@ -1,31 +1,52 @@
 import 'package:flutter/material.dart';
-import 'package:moody/bloc/mood/mood_bloc.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SettingsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Settings'),
-      ),
-      body: ListView(
-        children: [
-          ListTile(
-            title: Text('Delete User Data'),
-            subtitle: Text('Permanently deletes all saved mood entries.'),
-            onTap: () {},
+      body: CustomScrollView(
+        slivers: [
+          // Expanding SliverAppBar to enable one handed use on big smartphones.
+          SliverAppBar(
+            stretch: true,
+            expandedHeight: 300,
+            backgroundColor: Colors.transparent,
+            snap: false,
+            pinned: true,
+            floating: false,
+            flexibleSpace: FlexibleSpaceBar(
+              stretchModes: const <StretchMode>[
+                StretchMode.fadeTitle,
+              ],
+              title: Text(
+                'Settings',
+                style: Theme.of(context).textTheme.headline4,
+              ),
+            ),
           ),
-          ListTile(
-            title: Text('Export CSV'),
-            subtitle: Text('Coming soon: export your entries to a CSV file'),
-          ),
-          SwitchListTile(
-            title: Text('Dark Mode'),
-            subtitle: Text('Override the system theme.'),
-            // to be changed in DarkMode ticket.
-            value: true,
-            onChanged: (bool value) {},
+          SliverList(
+            delegate: SliverChildListDelegate(
+              [
+                ListTile(
+                  title: Text('Delete User Data'),
+                  subtitle: Text('Permanently deletes all saved mood entries.'),
+                  onTap: () {},
+                ),
+                ListTile(
+                  enabled: false,
+                  title: Text('Export CSV'),
+                  subtitle:
+                      Text('Coming soon: export your entries to a CSV file'),
+                ),
+                SwitchListTile(
+                  title: Text('Dark Mode'),
+                  subtitle: Text('Override the system theme.'),
+                  // to be changed in DarkMode ticket.
+                  value: true,
+                  onChanged: (bool value) {},
+                ),
+              ],
+            ),
           ),
         ],
       ),
